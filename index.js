33c3cffd9a589d2af8fe18041bd2bb108ac38387
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-11-11 16:10:12 +0100

    Second commit in PR

---
+++ index.js
@@ -0,0 +1,35 @@
+var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});
+
+
+function pickNumber() {
+  let i = 0;
+  i = i++; // Noncompliant; i is still zero
+
+  return i++; // Noncompliant; 0 returned
+}
+
+if (x) {
+  function foo() {} //foo is hoisted in Chrome, Firefox and Safari, but not in Edge.
+}
+
+if (x) {
+  function foo() {} //foo is hoisted in Chrome, Firefox and Safari, but not in Edge.
+}
+
+if (x) {
+  function foo() {} //foo is hoisted in Chrome, Firefox and Safari, but not in Edge.
+}
+
+if (x) {
+  function foo() {} //foo is hoisted in Chrome, Firefox and Safari, but not in Edge.
+}
+
+if (x) {
+  function foo() {} //foo is hoisted in Chrome, Firefox and Safari, but not in Edge.
+}
+
+if (x) {
+  function foo() {} //foo is hoisted in Chrome, Firefox and Safari, but not in Edge.
+}
